ae5f952bd3fc68e71665010e8f49328bfb687561
Shohei Yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2024-12-15 17:46:07 +0900

    Changed dark mode strategy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	darkMode: "selector",
 	theme: {
 		backgroundColor: {
 			light: "#C9D6FF",
