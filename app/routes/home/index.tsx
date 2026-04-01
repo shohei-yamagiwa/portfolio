48eb63a5834550f98ce333e34a7fc720463e3eba
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-04-02 01:48:12 +0900

    feat: implement async dataa loader for personal info

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,15 +1,23 @@
 import { Footer } from "~/components/footer";
 import { Header } from "~/components/header";
 import type { Route } from "./+types";
+import { personalInfoRepository } from "~/global";
+import { Hero } from "./components/hero";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Shohei Yamagiwa" }, { name: "description", content: "Shohei Yamagiwa's portfolio" }];
 }
 
-export default function Home() {
+export async function loader({}: Route.LoaderArgs) {
+  const personalInfo = await personalInfoRepository.getPersonalInfo();
+  return personalInfo;
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
   return (
     <div className="min-h-screen bg-white font-sans text-gray-900 selection:bg-blue-200">
       <Header />
+      <Hero {...loaderData} />
       <Footer />
     </div>
   );
